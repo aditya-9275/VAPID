03bd384d03710aac9f15a7073b1ae3f50aa77532
aditya-9275 <adityatomar.naps@gmail.com>
2025-04-15 13:32:14 +0530

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const webpush = require('web-push');
+
+// Generate VAPID keys
+const vapidKeys = webpush.generateVAPIDKeys();
+
+console.log("🔐 Public VAPID Key:\n", vapidKeys.publicKey);
+console.log("🗝️ Private VAPID Key:\n", vapidKeys.privateKey);
 
